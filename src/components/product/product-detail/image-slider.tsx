847bf561152c0b124ae885363da31fa945b5b2d5
Aayush Pradhan <muma5581@gmail.com>
2025-04-27 10:48:34 +0545

    e commerce

--- src/components/product/product-detail/image-slider.tsx
+++ src/components/product/product-detail/image-slider.tsx
@@ -1,52 +1,55 @@
 'use client'
 
 import React from 'react'
-const  Slider  =  dynamic(()=>import("react-slick"),{
-  ssr:false
-});
-
+import dynamic from 'next/dynamic'
+import Image from 'next/image'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Image from 'next/image'
-import dynamic from 'next/dynamic';
+
+const Slider = dynamic(() => import('react-slick'), {
+  ssr: false
+});
+
 interface IProps {
-    images:string[],
+  images: string[];
 }
 
-const ImageSlider:React.FC<IProps> = ({images}) =>{
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay:true,
-        autoplaySpeed: 2000,
-        swipeToSlide: true,
-      };
+const ImageSlider: React.FC<IProps> = ({ images }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    swipeToSlide: true,
+  };
 
-      console.log(images)
-    return(
-        <div className="h-[550px] w-[550px] overflow-hidden">
-      <Slider className='h-full' {...settings}>
-        {images?.map((image,index) => {
-                console.log(images)
-            return(
-                <div  key={index} className='h-[550px] w-[550px] '>
-                    <Image
-                    src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${image}`}
-                    width={1000}
-                    height={1000}
-                    alt={'product detail image'}
-                    className='w-[400px] h-[400px] object-cover'
-                    />
-                    
-                </div>
-            )
+  return (
+    <div className="h-[550px] w-[550px] overflow-hidden">
+      <Slider className="h-full" {...settings}>
+        {images?.map((image, index) => {
+          const fixedImageUrl = image.replace(/\\/g, '/');
+          const imageSrc = fixedImageUrl
+            ? `${process.env.NEXT_PUBLIC_API_BASE_URL}${fixedImageUrl}`
+            : '/products/pashminap.jpg';
+
+          return (
+            <div key={index} className="h-[550px] w-[550px]">
+              <Image
+                src={imageSrc}
+                width={1000}
+                height={1000}
+                alt="product detail image"
+                className="w-full h-full object-cover"
+              />
+            </div>
+          );
         })}
       </Slider>
     </div>
-    )
+  );
 }
 
 export default ImageSlider;
